3f03213aa30e81d764c722c326130bb5d568b383
Andrii Zakharenko <andriizakharenko@gmail.com>
2025-02-12 23:53:08 +0200

    feat-liveblocks: room updated

--- app/Room.tsx
+++ app/Room.tsx
@@ -2,10 +2,17 @@
 
 import { RoomProvider } from "@/liveblocks.config";
 import { ClientSideSuspense } from "@liveblocks/react";
+import { LiveMap } from "@liveblocks/client";
 
 const Room = ({ children }: { children: React.ReactNode }) => {
   return (
-    <RoomProvider id="my-room" initialPresence={{}}>
+    <RoomProvider
+      id="my-room"
+      initialPresence={{}}
+      initialStorage={() => ({
+        canvasObjects: new LiveMap<string, never>(),
+      })}
+    >
       <ClientSideSuspense fallback={<div>Loading...</div>}>
         {() => children}
       </ClientSideSuspense>
