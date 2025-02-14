de9dadc6e434ef0794642618b3c8c3ab7923b422
Andrii Zakharenko <andriizakharenko@gmail.com>
2025-02-14 23:52:19 +0200

    feat-live-cursors: vercel fail updates

--- components/Live.tsx
+++ components/Live.tsx
@@ -6,6 +6,7 @@ import { useCallback } from "react";
 
 const Live = () => {
   const others = useOthers();
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars
   const [{ cursor }, updateMyPresence] = useMyPresence() as any;
 
   const handlePointerMove = useCallback((event: React.PointerEvent) => {
@@ -15,7 +16,7 @@ const Live = () => {
     const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
 
     updateMyPresence({ cursor: { x, y } });
-  }, []);
+  }, [updateMyPresence]);
 
   const handlePointerLeave = useCallback((event: React.PointerEvent) => {
     event.preventDefault();
@@ -24,14 +25,14 @@ const Live = () => {
       cursor: null,
       message: null,
     });
-  }, []);
+  }, [updateMyPresence]);
 
   const handlePointerDown = useCallback((event: React.PointerEvent) => {
     const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
     const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
 
     updateMyPresence({ cursor: { x, y } });
-  }, []);
+  }, [updateMyPresence]);
 
   return (
     <div
--- components/cursor/Cursor.tsx
+++ components/cursor/Cursor.tsx
@@ -7,7 +7,7 @@ type Props = {
   message?: string;
 };
 
-const Cursor = ({ color, x, y, message }: Props) => (
+const Cursor = ({ color, x, y }: Props) => (
   <div
     className="pointer-events-none absolute left-0 top-0"
     style={{ transform: `translateX(${x}px) translateY(${y}px)` }}
